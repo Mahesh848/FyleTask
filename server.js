ff2cd812635975a62c50eb20e65bfbbe2afa8984
Mahesh Ippili <ippilimahesh1999@gmail.com>
2019-01-27 13:54:28 +0530

    project completed

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + './dist/fyletask'));
+app.use(express.static(__dirname+'/dist/Fyletask'));
 
 app.listen(process.env.PORT || 1848, () => {
   console.log('server listening on port 1848');
 });
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/fyletask/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Fyletask/index.html'));
 });
 
 
