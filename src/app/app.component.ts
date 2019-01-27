dab3c2507e6c963edaea8986d42eca4b161a7391
Mahesh Ippili <ippilimahesh1999@gmail.com>
2019-01-27 10:26:51 +0530

    Frontend Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Fyletask';
+  public navitem = 1;
+
+  ngOnInit() {
+    $(document).ready(function() {
+      $('.fa-bars').click(function() {
+        $('header').toggleClass('shown');
+        console.log($('header'));
+      });
+    });
+  }
+
+  changeNavitem(navitem) {
+    this.navitem = navitem;
+  }
 }
